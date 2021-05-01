f6e570363796ea54af0447f3fc170bc8a852fe48
Oo25251325 <tranlamanhduy1996@gmail.com>
2021-05-01 14:59:56 +0700

    Change About

--- CIS/Controllers/HomeController.cs
+++ CIS/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CIS.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Action About";
 
             return View();
         }
